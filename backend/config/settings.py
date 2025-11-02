19eb1c8272abc21be81f0981034ab117c80effc4
Victor <victor.loaiza2807@gmail.com>
2025-11-02 16:29:32 -0300

    chore(api)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -118,23 +118,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        # Lee la variable DATABASE_URL del entorno de Render
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Lee la variable DATABASE_URL del entorno de Render
+#         default=os.environ.get('DATABASE_URL')
+#     )
+# }
 
 
 # Password validation
